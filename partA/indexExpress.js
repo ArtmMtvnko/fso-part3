d73095b8e5e494db6b4c24ec63436a9b41bf6718
Artem <artemm3note@gmail.com>
2024-03-03 00:24:53 +0200

    Used postman for testing requests. Added checks in 'POST' request, now you can't add note without content.

--- partA/indexExpress.js
+++ partA/indexExpress.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
         id: 1,
@@ -35,8 +37,43 @@ app.get('/api/notes/:id', (request, response) => {
         response.json(note)
     } else {
         // response.statusMessage = 'some aboba text'
-        response.status(404).end('some other aboba text')
+        response.status(404).end('Cannot find note')
     }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    
+    const note = {
+        content: body.content,
+        important: Boolean(body.important) || false,
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+
+    console.log(note)
+    response.json(note)
 })
 
 const PORT = 3001
