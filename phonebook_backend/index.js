3cb84cd552269b8ba9764987653146ce910a7c57
Artem <artemm3note@gmail.com>
2024-03-05 22:22:37 +0200

    Done with 3.9 exercise. Now requests work properly (without cors error)

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -29,6 +29,10 @@ morgan.token('body', (req, res) => {
     return JSON.stringify(req.body)
 })
 
+const cors = require('cors')
+
+app.use(cors())
+
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
