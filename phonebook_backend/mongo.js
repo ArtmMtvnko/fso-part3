ad0d595d3d1498fc22ac54c5bb54607f7c5c4a4d
Artem <artemm3note@gmail.com>
2024-03-07 17:35:49 +0200

    renamed entrie to person

--- phonebook_backend/mongo.js
+++ phonebook_backend/mongo.js
@@ -13,26 +13,26 @@ mongoose.set('strictQuery', false)
 
 mongoose.connect(uri)
 
-const entrieSchema = new mongoose.Schema({
+const personSchema = new mongoose.Schema({
     name: String,
     number: String
 })
 
-const Entrie = mongoose.model('Entrie', entrieSchema)
+const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    Entrie.find({}).then(result => {
+    Person.find({}).then(result => {
         console.log('Phonebook:')
-        result.forEach(entrie => console.log(entrie.name, entrie.number))
+        result.forEach(person => console.log(person.name, person.number))
         mongoose.connection.close()
     })
 } else {
-    const entrie = new Entrie({
+    const person = new Person({
         name: process.argv[3],
         number: process.argv[4]
     })
     
-    entrie.save().then(result => {
+    person.save().then(result => {
         console.log('Entrie saved!')
         mongoose.connection.close()
     })
